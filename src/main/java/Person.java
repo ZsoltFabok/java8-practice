0198f7bb486548eeca45dace94cb7f8b7c9e6def
petersomogyi <sompi90@gmail.com>
2016-05-29 13:14:47 +0200

    Add equals and hashCode for Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,27 @@
 public class Person {
-	String name;
-	Integer age;
+    String name;
+    Integer age;
 
-	Person(String name, Integer age) {
-		this.name = name;
-		this.age = age;
-	}
+    Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return age != null ? age.equals(person.age) : person.age == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        return result;
+    }
 }
